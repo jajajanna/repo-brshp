2965582e51c4f40d3a1a695980cbe56b0df8ad4f
jajajanna <jannaculdora@gmail.com>
2025-05-22 19:09:59 +0800

    fist js

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Auth Elements
+        const authModal = document.getElementById('authModal');
+        const blurOverlay = document.getElementById('blurOverlay');
+        const closeModal = document.querySelector('.close-modal');
+        const tabBtns = document.querySelectorAll('.tab-btn');
+        const authForms = document.querySelectorAll('.auth-form');
+        const loginForm = document.getElementById('loginForm');
+        const signupForm = document.getElementById('signupForm');
+        const loginSubmit = document.getElementById('loginSubmit');
+        const signupSubmit = document.getElementById('signupSubmit');
+        const checkBookingBtn = document.getElementById('checkBookingBtn');
+        const bookingModal = document.getElementById('bookingModal');
+        const bookingDetails = document.getElementById('bookingDetails');
+        const bookingClose = document.querySelector('.booking-close');
+
+            });
